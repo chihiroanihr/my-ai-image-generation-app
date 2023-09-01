5dd7bb24a2d86b70bc14ff5b1fee1286ea924d10
Rhina Kim <chihiroanihr@gmail.com>
2023-08-31 22:08:38 -0400

    Update App.jsx: Navbar and Main Content (with routes to Home.jsx and CreatePost.jsx) set.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,50 @@
+import { twMerge } from "tailwind-merge";
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+import { Home, CreatePost } from "./pages";
+import { logo } from "./assets";
+
 const App = () => {
-  return <h1 className="text-3xl font-bold underline">Hello World!</h1>;
+  return (
+    <BrowserRouter>
+      {/* Navbar */}
+      <header
+        className={twMerge(
+          "flex items-center justify-between",
+          "w-full bg-white px-4 py-4 sm:px-8",
+          "border-b border-b-[#e6ebf4]",
+        )}
+      >
+        {/* Logo */}
+        <Link to="/">
+          <img src={logo} alt="logo" className="w-28 object-contain" />
+        </Link>
+
+        {/* Create Post Button */}
+        <Link
+          to="/create-post"
+          className={twMerge(
+            "font-iter font-medium text-white",
+            "rounded-md bg-[#6469ff] px-4 py-2",
+          )}
+        >
+          Create
+        </Link>
+      </header>
+
+      {/* Main Content */}
+      <main
+        className={twMerge(
+          "min-h-[calc(100vh-73px)]", // {screen height} - {navbar height}
+          "w-full bg-[#f9fafe] px-4 py-8 sm:p-8",
+        )}
+      >
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/create-post" element={<CreatePost />} />
+        </Routes>
+      </main>
+    </BrowserRouter>
+  );
 };
 
 export default App;
