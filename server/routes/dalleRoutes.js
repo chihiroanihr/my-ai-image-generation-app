522da8393335b5f54c7d18496d3226252fe8c36c
Rhina Kim <chihiroanihr@gmail.com>
2023-09-01 21:18:21 -0400

    Update dalleRoutes.js : POST to call to the OpenAI based on the prompts
    Functionality added which makes a call to the OpenAI DALL-E API based on the given prompt from client-side and returns AI generated result images.

--- server/routes/dalleRoutes.js
+++ server/routes/dalleRoutes.js
@@ -18,4 +18,33 @@ router.route("/").get((req, res) => {
   res.send("Hello from DALL-E");
 });
 
+/**
+ * Talk to OpenAI DALL-E API
+ * (Make a call to the OpenAI DALL-E API and based on the prompt, it will return AI-generated result images.)
+ */
+router.route("/").post(async (req, res) => {
+  try {
+    // Obtain prompt from the client
+    const { prompt } = req.body;
+
+    // Generate image
+    const aiResponse = await openai.createImage({
+      prompt,
+      n: 1, // only 1 image
+      size: "1024x1024",
+      response_format: "b64_json",
+    });
+
+    // Obtain the result image
+    const image = aiResponse.data.data[0].b64_json; // verify via logging!
+
+    // Send back to the client
+    res.status(200).json({ photo: image });
+  } catch (error) {
+    // Error
+    console.log(error);
+    res.status(500).send(error?.response.data.error.message); // verify via logging!
+  }
+});
+
 export default router;
