5731c7a6f6aa2238da2fa4c6f89c424c4d908510
Rhina Kim <chihiroanihr@gmail.com>
2023-09-01 23:03:35 -0400

    Bug Fix on routes/dalleRoutes.js

--- server/routes/dalleRoutes.js
+++ server/routes/dalleRoutes.js
@@ -1,6 +1,6 @@
 import express from "express";
 import * as dotenv from "dotenv";
-import { OpenAI } from "openai";
+import OpenAI from "openai";
 
 /**
  * Endpoint to generate the (image) data from the OpenAI DALL-E API.
@@ -28,7 +28,7 @@ router.route("/").post(async (req, res) => {
     const { prompt } = req.body;
 
     // Generate image
-    const aiResponse = await openai.createImage({
+    const aiResponse = await openai.images.generate({
       prompt,
       n: 1, // only 1 image
       size: "1024x1024",
@@ -36,14 +36,14 @@ router.route("/").post(async (req, res) => {
     });
 
     // Obtain the result image
-    const image = aiResponse.data.data[0].b64_json; // verify via logging!
+    const image = aiResponse.data[0].b64_json; // verify via logging!
 
     // Send back to the client
     res.status(200).json({ photo: image });
   } catch (error) {
     // Error
-    console.log(error);
-    res.status(500).send(error?.response.data.error.message); // verify via logging!
+    console.log("[LOG] ", error);
+    res.status(500).send(error); // verify via logging!
   }
 });
 
