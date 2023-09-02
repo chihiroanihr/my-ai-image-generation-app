41211bf1c1cc158b810be3b0cfd7127fc8350e3b
Rhina Kim <chihiroanihr@gmail.com>
2023-09-01 20:08:21 -0400

    Minor Update connect.js: docString

--- server/database/connect.js
+++ server/database/connect.js
@@ -1,9 +1,13 @@
 import mongoose from "mongoose";
 
-// Connect the application to Mongo DB
+/**
+ * Connect the application to Mongo DB
+ *
+ * @param {string} url - A MongoDB connection string.
+ */
 const connectDB = (url) => {
   mongoose.set("strictQuery", true); // useful when working with search functionality.
-  /**
+  /*
    * The strict option, (enabled by default), ensures that
    * values passed to our model constructor that were not specified in our schema
    * do not get saved to the db.
