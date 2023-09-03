cc1ddcedd90b78e1ac101e9c1b84f05622a906a2
Rhina Kim <chihiroanihr@gmail.com>
2023-09-03 18:25:25 -0400

    Update postRoutes.js : Upload asset to cloudinary & Create post and store it in database completed
    1. Cloudinary Configured.
    
    2. Route to get all created posts from database completed.
    
    3. route to upload asset (generated image) to Cloudinary & create a post and store it in database completed.

--- server/routes/postRoutes.js
+++ server/routes/postRoutes.js
@@ -13,4 +13,57 @@ dotenv.config();
 
 const router = express.Router();
 
+// Configure Cloudinary
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+/**
+ * Get all posts from database.
+ */
+router.route("/").get(async (req, res) => {
+  try {
+    const posts = await Post.find({});
+    // console.log(posts)
+
+    // Send back to the client
+    res.status(200).json({ success: true, data: posts });
+  } catch (error) {
+    // Error
+    console.log("[LOG] ", error);
+    res.status(500).json({ success: false, message: error });
+  }
+});
+
+/**
+ * 1. Upload asset (generated image) to Cloudinary.
+ * 2. Create a post and store it in the database.
+ */
+router.route("/").post(async (req, res) => {
+  try {
+    // Obtain generated image info & prompt from the client
+    const { name, prompt, photo } = req.body;
+
+    // Upload asset to cloudinary
+    const photoUrl = await cloudinary.uploader.upload(photo);
+
+    // Create new Post and add it in database
+    const newPost = await Post.create({
+      name,
+      prompt,
+      photo: photoUrl.url, // img asset from cloudinary
+    });
+    // console.log(newPost); // values stored: name, prompt, photo, _id, _v
+
+    // Send back to the client
+    res.status(201).json({ success: true, data: newPost });
+  } catch (error) {
+    // Error
+    console.log("[LOG] ", error);
+    res.status(500).json({ success: false, message: error });
+  }
+});
+
 export default router;
