e7efdc33b9452e85e79c6c6b7cf8524b418ec8aa
Rhina Kim <chihiroanihr@gmail.com>
2023-09-03 22:14:58 -0400

    Minor Update client/constants/serverUrl.js : Comments added

--- client/src/constants/serverUrl.js
+++ client/src/constants/serverUrl.js
@@ -4,3 +4,5 @@ export const SERVER_URL =
   import.meta.env.MODE === "development"
     ? `http://localhost:${PORT}`
     : "https://dall-e-0vef.onrender.com";
+
+// https://vitejs.dev/guide/env-and-mode.html
